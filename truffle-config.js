6ba1c379f5eedec687053d33c8edd9afa4450b04
Vairamuthu <mvairamuthu2003@gmail.com>
2023-01-02 15:55:06 +0530

    updated the wagmi connection with the frontend

--- truffle-config.js
+++ truffle-config.js
@@ -1,22 +1,28 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const fs = require('fs');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
 module.exports = {
   networks: {
     loc_development_development: {
       network_id: "*",
       port: 7545,
-      host: "127.0.0.1"
+      host: "127.0.0.1",
     },
     "inf_Media-dapp_goerli": {
       network_id: 5,
       gasPrice: 10,
-      provider: new HDWalletProvider(fs.readFileSync('c:\\Users\\mvair\\OneDrive\\Documents\\instagram-clone\\build\\contracts\\media.env', 'utf-8'), "https://goerli.infura.io/v3/57df90d6aba449359ddb768dd5fa36d0")
-    }
+      provider: new HDWalletProvider(
+        fs.readFileSync(
+          "c:\\Users\\mvair\\OneDrive\\Documents\\instagram-clone\\build\\contracts\\media.env",
+          "utf-8"
+        ),
+        "https://goerli.infura.io/v3/57df90d6aba449359ddb768dd5fa36d0"
+      ),
+    },
   },
   mocha: {},
   compilers: {
     solc: {
-      version: "0.8.17"
-    }
-  }
+      version: "0.8.17",
+    },
+  },
 };
